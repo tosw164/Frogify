4028e5889abf107045703ae9af665f7f3592e16a
Theo Oswandi <theooswandi@Theos-MacBook-Pro.local>
2017-10-04 10:06:40 +1300

    Created circle with visible range indicator for swinging
    - Created the assets to represent anchor and visible range
    - Created tongue swinging script TODO: automovement when swinging
    - Created a red reset material for right side wall

---
+++ Assets/Standard Assets/2D/Scripts/TongueSwing.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TongueSwing : MonoBehaviour {
+
+	Vector3 anchor_position;
+	DistanceJoint2D joint;
+	RaycastHit2D raycast_hit;
+	public LayerMask layer_mask;
+	public float distance = 3f;
+	public Transform anchor;
+
+	// Use this for initialization
+	void Start () {
+		joint = GetComponent<DistanceJoint2D>();
+		joint.enabled = false;
+		anchor_position = anchor.position;
+
+		Debug.Log (anchor_position);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown(KeyCode.Space)){
+			anchor_position = anchor.position;
+
+			raycast_hit = Physics2D.Raycast (transform.position, 
+				anchor_position - transform.position,
+				distance,
+				layer_mask);
+			Debug.Log (raycast_hit.collider != null);
+			Debug.Log (transform.position + " " + anchor_position + " " + (raycast_hit.collider != null));
+
+			if (raycast_hit.collider != null 
+				&& raycast_hit.collider.gameObject.GetComponent<Rigidbody2D>() != null){
+				joint.enabled = true;
+				joint.connectedBody = raycast_hit.collider.gameObject.GetComponent<Rigidbody2D> ();
+//				joint.distance = Vector2.Distance (transform.position, raycast_hit.point);
+				joint.distance = distance;
+			}
+			
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space)){
+			joint.enabled = false;
+		}
+	
+	}
+}
