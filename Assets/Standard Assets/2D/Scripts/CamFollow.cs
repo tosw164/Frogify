0914200d9f7982a689819f65c0192c9aa79eae09
Theo Oswandi <theooswandi@wifi-172-23-103-177.uoa-wifi.auckland.ac.nz>
2017-10-03 19:17:05 +1300

    Added reference on where camera follow logic was obtained

--- Assets/Standard Assets/2D/Scripts/CamFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CamFollow.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Obtained from http://answers.unity3d.com/questions/878913/how-to-get-camera-to-follow-player-2d.html
+
 public class CamFollow : MonoBehaviour {
 
 	public Transform player;
