a788e3649239de9fd55ad0fa438eccb77f0413e5
Theo Oswandi <theooswandi@wifi-172-23-103-177.uoa-wifi.auckland.ac.nz>
2017-10-03 19:17:38 +1300

    Modified right block to be red and reset the level

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -2,16 +2,13 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace UnityStandardAssets._2D
-{
-    public class Restarter : MonoBehaviour
-    {
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.tag == "Player")
-            {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
-            }
-        }
-    }
+//Obtained from https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnCollisionEnter2D.html
+
+public class Restarter : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+
+	}
 }
+
