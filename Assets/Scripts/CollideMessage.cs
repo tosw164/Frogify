c048f5f958a5e703bae81868d5aa9d2804230b89
Edison Rho <veryshortemailaddress@gmail.com>
2017-10-04 09:46:58 +1300

    Initial commit for the dialogue interaction. The commit includes the Fungus third-party dialogue framework API which is to be used for dialogue interactions. The commit also includes skeletons for the start memu, basic dialogue experimentation, puase menu, and start of the ending menu.

---
+++ Assets/Scripts/CollideMessage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class CollideMessage : MonoBehaviour {
+
+	public int debug;
+	public bool isRepeatable = true;
+
+	void OnCollisionEnter(Collision collision){
+		print ("collided");
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		print ("collided");
+
+		if (other.name == "Player" && isRepeatable) {
+			print ("triggered");
+			Flowchart.BroadcastFungusMessage("PINK_COLLIDED");
+			debug=2;
+			isRepeatable = false;
+
+		
+		}
+
+	}
+		
+}
