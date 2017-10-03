ade0279e41f045050e7546af39e83ff85ec94c96
Theo Oswandi <theooswandi@wifi-172-23-103-177.uoa-wifi.auckland.ac.nz>
2017-10-03 19:02:45 +1300

    Removed example project camera scripts and added my own

---
+++ Assets/Standard Assets/2D/Scripts/CamFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x, player.position.y, -6);
+	}
+}
