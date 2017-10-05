8df99ea28a8b70ca2fba065768082d25ca361dec
Seoyoung Choi <seoyoungc@gmail.com>
2017-10-05 16:30:15 +1300

    Added Spider and Needle Enemies
    Added Spider, which drops with acceleration. NeedleShooter shoots Needle Projectiles.
    Prefab for Needle added to keep shooting continuous.

---
+++ Assets/Needle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Needle : MonoBehaviour {
+
+
+	public Transform transform;
+	public float limit_x;
+	public float movement_speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > limit_x) {
+			Destroy (this.gameObject);
+		} else {
+			transform.Translate (Vector2.right * (movement_speed) * Time.deltaTime);
+		}
+	}
+}
