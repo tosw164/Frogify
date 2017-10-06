93f39f97c0c3489b1d168a420ca4363d606fea32
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-06 15:58:52 +1300

    Add collectable script to player, with basic worm prefab
    On collision with a worm (or any object tagged with Collectable), the
    object will be destroyed and the score incremented for the player.
    
    Currently, this score is bound to the player instance, will need to move
    to a singleton instance.

---
+++ Assets/Scripts/Collectables/PlayerCollectable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerCollectable : MonoBehaviour {
+
+
+	private int score = 0;
+	public Text scoreText;
+
+	void Start() {
+		UpdateScore ();
+	}
+
+	void Update() {
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Collectable")) {
+			other.gameObject.SetActive (false);
+			score++;
+			UpdateScore ();
+		}
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score.ToString ();
+	}
+}
