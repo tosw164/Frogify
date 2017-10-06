065785954722c15dcf6af310f6af006606a86060
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-06 16:39:29 +1300

    Link the player collectable script to global singleton

--- Assets/Scripts/Collectables/PlayerCollectable.cs
+++ Assets/Scripts/Collectables/PlayerCollectable.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class PlayerCollectable : MonoBehaviour {
 
 
-	private int score = 0;
+//	private int score = 0;
 	public Text scoreText;
 
 	void Start() {
@@ -18,13 +18,13 @@ public class PlayerCollectable : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag ("Collectable")) {
 			other.gameObject.SetActive (false);
-			score++;
+			ScoreManager.manager.score++;
 			UpdateScore ();
 		}
 	}
 
 	void UpdateScore()
 	{
-		scoreText.text = "Score: " + score.ToString ();
+		scoreText.text = "Score: " + ScoreManager.manager.score;
 	}
 }
---
+++ Assets/Scripts/Collectables/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	public static ScoreManager manager;
+
+	public int score;
+
+	void Awake () {
+		if (manager == null) {
+			DontDestroyOnLoad (gameObject);
+			manager = this;
+		}
+		else if (manager != this) {
+			Destroy (gameObject);
+		}
+	}
+}
