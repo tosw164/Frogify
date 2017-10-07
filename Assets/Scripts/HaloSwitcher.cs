a90c99b63f498a7b38ecbaca7837f11d65229c6f
dylHall <dylan.hall077@gmail.com>
2017-10-07 15:10:08 +1300

    Made the halo effect work based on trigger event with a 2D colider on an object

---
+++ Assets/Scripts/HaloSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaloSwitcher : MonoBehaviour {
+
+	private Behaviour componentHalo;
+	private int i;
+	// Use this for initialization
+	void Start () {
+		i = 0;
+		Debug.Log("made i: "+i);
+		componentHalo = (Behaviour)GetComponent("Halo");
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) 
+	{
+		i++;
+		Debug.Log("ouch"+i);
+		componentHalo.enabled = true;
+	}
+
+	void OnTriggerExit2D (Collider2D col) 
+	{
+		componentHalo.enabled = false;
+	}
+
+}
