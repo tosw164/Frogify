67a280de8416e1e51e5ef7a7eddf7d17db3d395f
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-07 17:04:57 +1300

    Clean up ScoreManager singleton script

--- Assets/Scripts/Collectables/ScoreManager.cs
+++ Assets/Scripts/Collectables/ScoreManager.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
+	
 	public static ScoreManager manager;
-
 	public int score;
 
 	void Awake () {
+
+		// Ensure singleton existence of the ScoreManager
 		if (manager == null) {
 			DontDestroyOnLoad (gameObject);
 			manager = this;
