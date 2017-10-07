218e06cc1c7b4d8a66945cf9a56b4367f2d5b5a8
Seoyoung Choi <seoyoungc@gmail.com>
2017-10-07 14:41:54 +1300

    Added enemy sandbox, and spider also shakes

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -6,42 +6,68 @@ public class Spider : MonoBehaviour {
 	public Transform transform;
 	public float top_y;
 	public float bottom_y;
+	public float shakingLength;
+
 
 	private float movement_speed = 2f;
 	private bool move_down;
+	private bool shake;
+	private bool prevShake;
+	private float shakeCounterRate = 5;
+	private float shakeCounter;
 
+	//Counter for speed of spider dropping down, increments as it goes
 	float counter;
-
-
 
 	// Use this for initialization
 	void Start () {
+		shake = true;
+		prevShake = true;
 		move_down = true;
-		Debug.Log (transform.position.y);
-		top_y = 5f;
-		bottom_y = -2f;
 		counter = 0;
+		shakeCounter = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		Debug.Log (transform.position.y);
+		if (shake == true) {
+			shakeSpider ();
+		} else {
+			if (transform.position.y < bottom_y) {
+				move_down = false;
+			} else if (transform.position.y > top_y) {
+				shake = true;
+				shakeSpider ();
+				counter = 0.0f;
 
-		if (transform.position.y < bottom_y){
-			move_down = false;
-		} else if (transform.position.y > top_y) {
-			move_down = true;
-			counter = 0.0f;
+			}
+
+			counter = counter + (5 * Time.deltaTime);
+
+			if (move_down) {
+				transform.Translate (-Vector2.up * (movement_speed + counter * 5) * Time.deltaTime);
+			} else if (!move_down) {
+				transform.Translate (Vector2.up * movement_speed * Time.deltaTime);
+			}
 		}
 
-		counter = counter + (5*Time.deltaTime);
+	}
 
-		if (move_down){
-			transform.Translate (-Vector2.up * (movement_speed + counter) * Time.deltaTime);
-		} else if (!move_down) {
-			transform.Translate (Vector2.up * movement_speed * Time.deltaTime);
+
+	void shakeSpider(){
+		float speed = 1.0f; //how fast it shakes
+		float amount = 10.0f; //how much it shakes
+		shakeCounter++;
+		if (shakeCounter == shakeCounterRate) {
+			if (prevShake) {
+				transform.Translate (Vector2.left * (amount * (Time.deltaTime * speed)));
+				prevShake = false;
+			} else {
+				transform.Translate (Vector2.right * (amount * (Time.deltaTime * speed)));
+				prevShake = true;
+			}
+			shakeCounter = 0f;
 		}
-
 	}
 }
