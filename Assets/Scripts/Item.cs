f2c75a029585c4b44280156ae159b645671c746b
Edds <veryshortemailaddress@gmail.com>
2017-10-07 14:14:40 +1300

    Implement dialogue system (#20)
    Dialogue implementation complete for the prototype. However, need to do some referencing and repositioning once the full level is concrete (or at least the NPC area is complete).
    
    Bug with mashing on Esc key still exists, but a very insignificant edge case; will address in the future. (See #10)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Item class for item-collidable objects.
+ * Note this is distinct from the score items.
+ * 
+ * Consists of a basic OnTrigger collide event, where, when collided, 
+ * will add the item's name onto the inventory manager singleton class.
+ * 
+**/
+public class Item : MonoBehaviour {
+	public string itemName;
+
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {			
+			this.gameObject.SetActive(false);
+			ItemManager.itemManager.itemList.Add(itemName);//add to player's inventory
+		}
+	
+	}
+}
