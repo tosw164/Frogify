406613494c326e78eab615379fb8c64ea1e78ac8
Eli Salter <esal284@aucklanduni.ac.nz>
2017-10-08 21:54:20 +1300

    here you go eds x

---
+++ Assets/Scripts/EliPlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliPlayerScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(50, 50);
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+
+    }
+}
