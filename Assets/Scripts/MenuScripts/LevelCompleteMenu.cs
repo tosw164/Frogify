ccf1c6a715760cb29696ad78a038d0e5cc2b1d75
Edison Rho <veryshortemailaddress@gmail.com>
2017-10-08 22:02:27 +1300

    Added score interactin with menu.

--- Assets/Scripts/MenuScripts/LevelCompleteMenu.cs
+++ Assets/Scripts/MenuScripts/LevelCompleteMenu.cs
@@ -5,11 +5,16 @@ public class LevelCompleteMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		this.gameObject.SetActive (true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void setActive(string bleh){
+		Debug.Log ("hi");
+		this.gameObject.SetActive (false);
 	}
 }
