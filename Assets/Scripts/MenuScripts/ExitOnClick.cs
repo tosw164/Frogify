bf9da75a54c5472119cc1f06d843ec3825dcc031
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-09 20:45:53 +1300

    Commented out UnityEditor breaking build

--- Assets/Scripts/MenuScripts/ExitOnClick.cs
+++ Assets/Scripts/MenuScripts/ExitOnClick.cs
@@ -5,7 +5,7 @@ public class ExitOnClick : MonoBehaviour {
 
 	public void Quit(){
 //this is for when we are running on the unity editor.
-		UnityEditor.EditorApplication.isPlaying = false;
+//		UnityEditor.EditorApplication.isPlaying = false;
 		//if we want to do it differently on a web app (e.g. close tab) or something we'd do it differently.
 	}
 }
