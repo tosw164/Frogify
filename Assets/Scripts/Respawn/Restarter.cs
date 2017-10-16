04a8e5d9a70e5125ef86abf62475d28483d4e5ad
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-17 10:48:10 +1300

    Cleanup prototype (#55)
    After completing the prototype, the project was a bit messy. I have
    gone through and cleaned up some of the files, delete duplicates and
    unused files, as well as moving assets around for better cohesion,
    searchability etc.
    
    I have not touched most of the script, we will still need to tidy these
    up.

---
+++ Assets/Scripts/Respawn/Restarter.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Obtained from https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnCollisionEnter2D.html
+
+public class Restarter : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			SceneManager.LoadScene(3);
+
+	}
+}
+
