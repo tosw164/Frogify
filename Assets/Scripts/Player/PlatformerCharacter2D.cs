570807152d01548d72060b9823bfb7bc749517a5
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-20 20:01:39 +1300

    Moved pep to chimney in test swing scene

--- Assets/Scripts/Player/PlatformerCharacter2D.cs
+++ Assets/Scripts/Player/PlatformerCharacter2D.cs
@@ -35,9 +35,11 @@ namespace UnityStandardAssets._2D
 		private void FixedUpdate()
 		{
 			m_Grounded = false;
+
 			// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 			// This can be done using layers instead but Sample Assets will not overwrite your project settings.
 			Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+			
 
 			for (int i = 0; i < colliders.Length; i++)
 			{
