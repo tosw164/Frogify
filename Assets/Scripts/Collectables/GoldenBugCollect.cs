9d6f69f15658688cb55cd2273c5daf768d60885e
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-21 16:18:14 +1300

    Rename script that handles collectable pickups

--- Assets/Scripts/Collectables/GoldenBugCollect.cs
+++ Assets/Scripts/Collectables/GoldenBugCollect.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PlayerCollectable : MonoBehaviour {
+public class GoldenBugCollect : MonoBehaviour {
 
 	// Update the singleton ScoreManager's score when the player picks up a coin.
 	void OnTriggerEnter2D(Collider2D other) {
