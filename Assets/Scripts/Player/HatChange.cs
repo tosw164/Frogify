83471c659cb371ddb37acc3b69f8d77693a65eb3
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-21 19:44:15 +1300

    Added hats switchable with 1 and 2 key

---
+++ Assets/Scripts/Player/HatChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatChange : MonoBehaviour {
+
+	private SpriteRenderer renderer;
+	public Sprite[] hats;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Alpha1)){
+			renderer.sprite = hats [0];
+		} else if (Input.GetKeyDown(KeyCode.Alpha2)){
+			renderer.sprite = hats [1];
+		}
+	
+	}
+}
