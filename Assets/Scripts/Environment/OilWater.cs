7159b83a928893262592915bf85c2f071347d462
Edison Rho <veryshortemailaddress@gmail.com>
2017-10-21 21:37:38 +1300

    Added oily water damage; need the game manager to integrate.

---
+++ Assets/Scripts/Environment/OilWater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script for oily water.
+ * Whenever player is inside the water, the player gets damaged every second,
+ */
+public class OilWater : MonoBehaviour {
+	private bool _playerInWater = false;
+
+
+
+	void OnTriggerStay(Collider2D other) {
+		if (other.tag == "Player") {
+			_playerInWater = true;
+			//user manager to damage player
+		}
+
+	}
+
+
+}
