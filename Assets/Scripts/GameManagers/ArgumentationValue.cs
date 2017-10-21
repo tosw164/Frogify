56b4581fbda65b2b9e619c6af4ca26bdf77be16b
dylHall <dylan.hall077@gmail.com>
2017-10-21 15:07:54 +1300

    Added a series of enums in order to promote OO and increase encapsulation

---
+++ Assets/Scripts/GameManagers/ArgumentationValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace POCC
+{
+	/**
+	 * Simple enum for representing how/what argument was selected
+	 * in order to delgate different amounts of score.
+	 */
+	public enum ArgumentationValue
+	{
+		FIRST_ATTEMPT
+	}
+}
+
--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameManager : MonoBehaviour {
 	
@@ -21,6 +23,14 @@ public class GameManager : MonoBehaviour {
 	//via use of methods - just to help debugging/controlling the value.
 	private int health = DEFAULT_HEALTH;
 
+	//Values for each respective score.
+	private long argumentationScore = 0;
+
+	//THIS MIGHT BE REDUNDANT DUE TO COLLECTIBLES ELSEWHERE.
+	//Stored here to potentially facilitaed consolidation of scoring.
+	private long collectibleScore = 0;
+
+
 	//======================================================
 
 
@@ -36,6 +46,8 @@ public class GameManager : MonoBehaviour {
 	}
 	
 
+	//Update Methods:
+
 	public void incrementHealth(){
 		health++;
 	}
@@ -50,13 +62,50 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	//Getter method
+	/**
+	 * This should be called by other classes in order to reset health
+	 * back to default values.
+	 */
+	public void resetHealth(){
+		health = DEFAULT_HEALTH;	
+	}
+
+	public void incrementCollectableScore(POCC.Collectable collectable){
+		//If it is a normal gold fly, just increment by 1
+		if (collectable.Equals (POCC.Collectable.GOLDFLY)) {
+			collectibleScore++;
+		}
+	}
+
+	public void incrementArgumentationScore(POCC.ArgumentationValue argueVal){
+		//If it is a normal gold fly, just increment by 1
+		if (argueVal.Equals (POCC.ArgumentationValue.FIRST_ATTEMPT)) {
+			argumentationScore+=10;
+		}
+	}
+
+
+	//Getter Methods:
 	public float getHealth(){
 		return health;
 	}
 
 
+
+
+	//Helper method to switch scene when required.
 	public void switchScene(int sceneNum){
 		SceneManager.LoadScene (sceneNum);
+	}
+
+	//Skeleton of a method used for serialization.
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter();
+
+		//Using unity built in persistentDataPath in order to be more professional
+		FileStream file = File.Open(Application.persistentDataPath + "/pepInfo.dat", FileMode.Open);
+
+		//Now need to say WHAT data you want to save. You need an object you can write to the file… You need a CLEAN CLASS that will just contain data.
+
 	}
 }
