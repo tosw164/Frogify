874ef445b99da98bdf03cf6c535ada8170a0c181
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-22 00:35:24 +1300

    Add ScneeType enum
    This will be used in tandem with SceneLookup to resolve scene names from
    the enums.

---
+++ Assets/Scripts/GameManagers/SceneType.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace POCC {
+
+	public enum SceneType {
+		MAIN_MENU, GAME_OVER, END_LEVEL_SCENE, TUTORIAL_1, TUTORIAL_2, HUB_1,
+		LOGOS, HUB_2, BEARLANA, PATHOS_1, PATHOS_2, HUB_3, ETHOS_1, ETHOS_2,
+		HUB_4, END_OF_YEAR
+	}	
+}
