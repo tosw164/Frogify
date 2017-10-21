87effbe729a129d02bc0d7b0d31e3c5594a39aa2
dylHall <dylan.hall077@gmail.com>
2017-10-21 14:42:36 +1300

    Added POCC class and began GameManager implementation

---
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	
+	public static GameManager manager;
+
+
+	//======================================================
+	//Default Values
+	private static int DEFAULT_HEALTH = 3;
+	//======================================================
+
+
+
+	//======================================================
+	//Fields to be managed
+
+	//private just to enforce that health should only be incremented
+	//via use of methods - just to help debugging/controlling the value.
+	private int health = DEFAULT_HEALTH;
+
+	//======================================================
+
+
+
+
+	void Awake () {
+		if (manager == null) {
+			DontDestroyOnLoad (gameObject);
+			manager = this;
+		} else if (manager != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+
+	public void incrementHealth(){
+		health++;
+	}
+
+	public void decrementHealth(){
+		health--;
+		Debug.Log ("Took damage, current health is: " + health);
+		if (health == 0) {
+			health = DEFAULT_HEALTH;//set health BACK to default value
+			//TODO: SHould this be reset by the gameover screen?
+			switchScene(POCC.SceneLookup.GAME_OVER_INDEX);//Probably a good idea to CHANGE THIS. DONT MAKE IT FULLY INDEX BASED.
+		}
+	}
+
+	//Getter method
+	public float getHealth(){
+		return health;
+	}
+
+
+	public void switchScene(int sceneNum){
+		SceneManager.LoadScene (sceneNum);
+	}
+}
---
+++ Assets/Scripts/GameManagers/SceneLookup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POCC
+{
+	/**
+	 * This class will simply provide a means of looking 
+	 * up certain values in the project.
+	 */
+	public class SceneLookup
+	{
+		public static int MAIN_MENU_INDEX = 0;
+		public static int GAME_OVER_INDEX = 3;
+		public static int FIRST_LEVEL_INDEX= 2;
+
+
+		public SceneLookup ()
+		{
+				
+		}
+
+
+	}
+}
+
