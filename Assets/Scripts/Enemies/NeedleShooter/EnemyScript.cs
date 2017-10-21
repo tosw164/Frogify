0daa335e1512ed2ff8de1d843bdb3f3e030c2eeb
Eli Salter <esal284@aucklanduni.ac.nz>
2017-10-21 13:44:06 +1300

    Basic implementation of NeedleShooter, shoots needleShot in single direction. Needs integration with Health features in gameManager - current health manager is just for testing/sandbox level

---
+++ Assets/Scripts/Enemies/NeedleShooter/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+    private WeaponScript weapon;
+
+    void Awake()
+    {
+        // Retrieve the weapon only once
+        weapon = GetComponent<WeaponScript>();
+    }
+
+    void Update()
+    {
+        // Auto-fire
+        if (weapon != null && weapon.CanAttack)
+        {
+            weapon.Attack(true);
+        }
+    }
+}
