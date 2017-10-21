1fcc38e5eecffb8e3dc002568f780217adfb0b1e
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2017-10-21 21:06:00 +1300

    UI-Redisgn : Added UI
    Changes:
    1. Added health
    2. Added inventory
    3. Added collectables

---
+++ Assets/TextScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour {
+
+	Text score;
+
+	// Use this for initialization
+	void Start () {
+		score = GetComponent<Text> ();
+		score.text = "" + ScoreManager.manager.collectableScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score.text = "" + ScoreManager.manager.collectableScore;
+	}
+}
