e57faf0c8f806f43edb944c4b9aaa32755b8cfc0
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-21 14:41:02 +1300

    Changed mushroom to not have collisionbox and be based off trigger events

--- Assets/Scripts/Environment/BounceMushroomScript.cs
+++ Assets/Scripts/Environment/BounceMushroomScript.cs
@@ -7,32 +7,12 @@ public class BounceMushroomScript : MonoBehaviour {
 	GameObject Player;
 	Rigidbody2D rigi2d;
 	public Vector2 velocity;
-	//public Animator anim;
 	public bool collided;
 
-
-	// Use this for initialization
-	void Start () {
-		//anim = GetComponent<Animator> ();
-	}
-
-	void Update () {
-		if (collided) {
-			//anim.SetBool ("IsReleased",true);
-			Player.GetComponent<Rigidbody2D>().velocity = velocity;
-			//anim.SetBool ("IsOnTop",false);
-			collided = false;
-		}
-	}
-
-	void OnCollisionEnter2D(Collision2D other) {
+	//Add velocity to character if collide with boxcollider of mushroom
+	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("Player")) {
-			//anim.SetBool ("IsReleased",false);
-			//anim.SetBool ("IsOnTop",true);
-			collided = true;
-			Player = other.gameObject;
+			other.gameObject.GetComponent<Rigidbody2D>().velocity = velocity;
 		}
 	}
-		
-
 }
