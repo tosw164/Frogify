031db0f1aee169564b81d0ba759f0c3c6b01068c
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2017-10-21 21:28:45 +1300

    UI redesign - Added inventory
    Changes:
    1. Added inventory

--- Assets/InventoryDisplay.cs
+++ Assets/InventoryDisplay.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class InventoryDisplay : MonoBehaviour {
 
+	public string itemName;
+	public Renderer renderers;
+
 	// Use this for initialization
 	void Start () {
-	
+		renderers = GetComponent<Renderer> ();
+		renderers.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log(ItemManager.itemManager.itemList.Contains (itemName));
+		if (ItemManager.itemManager.itemList.Contains (itemName)) {
+			Debug.Log("pickedup");
+			renderers.enabled = true;
+		}
 	}
 }
