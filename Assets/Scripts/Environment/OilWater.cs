739c9555e47a1da7abb2816b6003e5ddb2565ba1
Edison Rho <veryshortemailaddress@gmail.com>
2017-10-22 11:31:46 +1300

    Ethos scene 2 basically finished; perhaps need some final tuning in terms of difficulty an decorations. Have to add the snake yang chase sequence.

--- Assets/Scripts/Environment/OilWater.cs
+++ Assets/Scripts/Environment/OilWater.cs
@@ -7,14 +7,36 @@ using System.Collections;
  */
 public class OilWater : MonoBehaviour {
 	private bool _playerInWater = false;
+	private float _timeInWater = 0f;
 
 
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			other.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.1f, 0.1f);
+			
+		}
+	}
 
-	void OnTriggerStay(Collider2D other) {
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			other.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		_timeInWater += Time.deltaTime;
+
 		if (other.tag == "Player") {
 			_playerInWater = true;
 			//user manager to damage player
+			if(_timeInWater >= 5) {
+				_timeInWater = 0;
+				POCC.GameManager.getInstance().decrementHealth();
+			}
 		}
+
+
 
 	}
 
