6a1aa4669759d03599120af1170333a9f27e58c5
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-21 16:30:50 +1300

    Change bug collectable script to be bound to collectables
    From learning that Pep has a lot of collision events (due to him, as the
    playable character, being in contact with the ground and walls and other
    environmental assets), it is better to have the collision event for
    Pep<->Collectable bug attached to the bug. I have refactored the current
    implementation so that on collision, the score is incremented and the
    bug destroys itself, instead of Pep destroying the collided bug.

--- Assets/Scripts/Collectables/GoldenBugCollect.cs
+++ Assets/Scripts/Collectables/GoldenBugCollect.cs
@@ -6,9 +6,9 @@ public class GoldenBugCollect : MonoBehaviour {
 
 	// Update the singleton ScoreManager's score when the player picks up a coin.
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.CompareTag ("Collectable")) {
-			Destroy (other.gameObject);
+		if (other.gameObject.CompareTag ("Player")) {
 			ScoreManager.manager.collectableScore++;
+			Destroy (gameObject);
 		}
 	}
 }
