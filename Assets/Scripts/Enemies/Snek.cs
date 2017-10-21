94961aed4a994f9cd073ced73bdd54ec72ed3e36
Seoyoung Choi <seoyoungc@gmail.com>
2017-10-21 18:18:11 +1300

    Added Snekyoung Script for movement

---
+++ Assets/Scripts/Enemies/Snek.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snek : MonoBehaviour {
+
+	public float endX;
+	public float endY;
+
+	public float speed = 1f;
+
+	Vector2 startPosition;
+	Vector2 endPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		endPosition = new Vector2 (endX, endY);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.Translate (Vector2.left * step);
+	}
+}
