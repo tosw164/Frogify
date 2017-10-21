663bdf207fb024b283dd539797b0776611bbcb31
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-21 15:24:23 +1300

    Added initial (buggy) reset y velocity to script

--- Assets/Scripts/Environment/BounceMushroomScript.cs
+++ Assets/Scripts/Environment/BounceMushroomScript.cs
@@ -12,7 +12,14 @@ public class BounceMushroomScript : MonoBehaviour {
 	//Add velocity to character if collide with boxcollider of mushroom
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("Player")) {
-			other.gameObject.GetComponent<Rigidbody2D>().velocity = velocity;
+			Rigidbody2D rb2d = other.gameObject.GetComponent<Rigidbody2D>();
+
+			Vector2 current_velocity = rb2d.velocity;
+			current_velocity.y = 0f;
+
+			rb2d.velocity = current_velocity;
+			rb2d.AddForce(velocity, ForceMode2D.Impulse);
+			// rb2d.velocity = velocity;
 		}
 	}
 }
