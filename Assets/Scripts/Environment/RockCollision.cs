7a2c8535ad9888fafffb11fb477098303e1ccfd9
Edison Rho <veryshortemailaddress@gmail.com>
2017-10-21 19:11:20 +1300

    Added vertical scene for part 2 of ethos scene.

---
+++ Assets/Scripts/Environment/RockCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockCollision : MonoBehaviour {
+	public int expiryTime;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, expiryTime);
+
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+
+		if (collision.gameObject.tag == "Player") {
+			Debug.Log ("HI");
+			Physics2D.IgnoreCollision (collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+		}
+	}
+}
