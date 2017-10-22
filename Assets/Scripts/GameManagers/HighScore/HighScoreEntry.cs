1a05bd6adec88cd1dd8f0d0896d47435b8472110
dylHall <dylan.hall077@gmail.com>
2017-10-23 00:18:17 +1300

    Added initial structure of the high score system

---
+++ Assets/Scripts/GameManagers/HighScore/HighScoreEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/**
+ * Simple structure class that maintains an entry for a highscore.
+ */
+namespace POCC.HighScore
+{
+	[Serializable]
+	public struct HighScoreEntry
+	{
+		public String _name;
+		public int _collectScore;
+		public int _argueScore;
+
+		public HighScoreEntry(String name, int collectScore, int argueScore){
+			_name = name;
+			_collectScore = collectScore;
+			_argueScore = argueScore;
+		}
+	}
+}
+
