0b000ad10d485c08b18068b5b94619d0a463b3c5
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-22 14:21:40 +1300

    Enhance GameManager; Implement factory scene generator/switcher (#76)
    I have implemented more OOP appropriate level factory generator and
    switcher, which utilises both factory and template patterns. Hooks can
    be added so that upon scene changes, we can execute code to reset game
    state, for example.
    
    There is also a Scene class hierarchy that has been introduced. The
    superclass Scene is subclassed (one per scene asset), and each child
    will have it's own hooks for start up and tear down so when the scene is
    switched in or out, we can execute code that will set the game state to
    something stable. These classes is in a sub namespace POCC.Scenes, and
    I've also refactored the other Scene related classes/enums in there too.
    
    An adapter SceneSwitcherAdapter was implemented so that the events
    within the scenes can use the scene switching mechanism.
    
    The prototype scenes (the 4 of them, I didn't include the swingtest
    scene) have been changed to use this new scene switching implementation.

--- Assets/Scripts/GameManagers/PlayerData.cs
+++ Assets/Scripts/GameManagers/PlayerData.cs
@@ -1,29 +1,30 @@
 ﻿using System;
+using POCC.Scenes;
 
 namespace POCC
 {
 	/**
 	 * Class used to hold the values that should be persisted
 	 * between executions. Simply a "data container"
-	 * 
+	 *
 	 * Reference: Unity Live Training 3 March 2014:
 	 * 		https://www.youtube.com/watch?v=J6FfcJpbPXE
-	 */ 
+	 */
 	[Serializable] //states that the class is serialziable
 	public class PlayerData
 	{
 		private int _health;
 		private long _argumentationScore;
 		private long _collectableScore;
-		private string _levelString;
+		private SceneType _level;
 
-		//List for achievement??
+		//TODO: Add fields regarding achievements
 
-		public PlayerData(int health, long aScore, long cScore, String levelString){
+		public PlayerData(int health, long aScore, long cScore, SceneType level){
 			_health = health;
 			_argumentationScore = aScore;
 			_collectableScore = cScore;
-			_levelString = levelString;
+			_level = level;
 		}
 
 		public int getHealth(){
@@ -38,8 +39,8 @@ namespace POCC
 			return _collectableScore;
 		}
 
-		public string getLevelString(){
-			return _levelString;
+		public SceneType getLevel(){
+			return _level;
 		}
 	}
 }
