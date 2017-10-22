29d1c40ed8ca809be064dbeb2d333685ea00e661
Eli Salter <esal284@aucklanduni.ac.nz>
2017-10-22 14:50:10 +1300

    Added basic Audio Manager that loops through all sounds in a list and instatiates AudioSource objects of them

---
+++ Assets/Scripts/Environment/Sound/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+/// <summary>
+/// Credit: https://www.youtube.com/watch?v=6OT43pvUyfY&t=41s
+/// </summary>
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
