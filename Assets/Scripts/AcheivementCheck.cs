a1df5404d290250d8ea7ae1492a7479dbfb8001f
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2017-10-22 17:27:29 +1300

    UI redesign - Achievement panel prefab added

---
+++ Assets/Scripts/AcheivementCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using POCC;
+
+public class AcheivementCheck : MonoBehaviour {
+
+	public string acheivementname;
+	private Image background;
+
+	// Use this for initialization
+	void Start () {
+		background = (Image)gameObject.transform.GetChild(3).GetComponent<Image>();
+		if (POCC.GameManager.getInstance ().getAchievements ().Contains (acheivementname)) {
+			background.enabled = true;
+		} else {
+			background.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
