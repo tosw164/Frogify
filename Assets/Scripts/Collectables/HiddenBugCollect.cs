2aa4fe7b37f4916260ad56d808be26411b126e71
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-24 02:47:07 +1300

    Add hidden bug prefab for achievements

---
+++ Assets/Scripts/Collectables/HiddenBugCollect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HiddenBugCollect : MonoBehaviour {
+
+	// Update the singleton POCC.GameManager's collectable score when the player picks up a coin.
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			POCC.GameManager.getInstance().sendAchievementEvent(POCC.Achievements.AchievementType.HIDDEN_ITEMS);
+			Destroy (gameObject);
+            FindObjectOfType<AudioManager>().Play("goldenFlyCollectable");
+
+        }
+	}
+}
