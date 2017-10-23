6f83e196cf2e92be65e1bdf120837ce5c9b04adc
dylHall <dylan.hall077@gmail.com>
2017-10-23 21:52:25 +1300

    Added achievement system fixes, and GameManager change to call on

---
+++ Assets/Scripts/GameManagers/Achievements/AchievementSenderAdapter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using POCC;
+using POCC.Achievements;
+using UnityStandardAssets._2D;
+
+public class AchievementSenderAdapter : MonoBehaviour {
+	
+	public void sendAchievement(int achievementCode) {
+		AchievementType achType = (AchievementType)achievementCode;
+		GameManager.getInstance().getAchievements(achType);
+	}
+}
