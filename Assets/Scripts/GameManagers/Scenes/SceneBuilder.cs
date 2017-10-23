94cb7bb00df3a84e7a22e6f68d688e99b01ccdc2
Kevin Hira <khir664@aucklanduni.ac.nz>
2017-10-24 01:51:33 +1300

    Add post hook to reset scores after end level scenes

--- Assets/Scripts/GameManagers/Scenes/SceneBuilder.cs
+++ Assets/Scripts/GameManagers/Scenes/SceneBuilder.cs
@@ -1,50 +1,56 @@
 using System;
 
 namespace POCC.Scenes {
-    public class SceneBuilder {
+	public class SceneBuilder {
 
-        private Scene _scene;
+		private Scene _scene;
 
-        public SceneBuilder(SceneType sceneType) : this(new Scene(sceneType, null)) {
+		public SceneBuilder(SceneType sceneType) : this(new Scene(sceneType, null)) {
 
-        }
+		}
 
-        public SceneBuilder(SceneType sceneType, string sceneLocation) : this(new Scene(sceneType, sceneLocation)) {
+		public SceneBuilder(SceneType sceneType, string sceneLocation) : this(new Scene(sceneType, sceneLocation)) {
 
-        }
+		}
 
-        public SceneBuilder(Scene scene) {
-            _scene = scene;
-        }
+		public SceneBuilder(Scene scene) {
+			_scene = scene;
+		}
 
-        public Scene build() {
-            return _scene;
-        }
+		public Scene build() {
+			return _scene;
+		}
 
-        public SceneBuilder setLocation(string sceneLocation) {
-            Action startupHooks = _scene.getStartupHooks();
-            Action teardownHooks = _scene.getTeardownHooks();
+		public SceneBuilder setLocation(string sceneLocation) {
+			Action startupHooks = _scene.getStartupHooks();
+			Action teardownHooks = _scene.getTeardownHooks();
 
-            _scene = new Scene(_scene.getSceneType(), sceneLocation);
+			_scene = new Scene(_scene.getSceneType(), sceneLocation);
 
-            _scene.appendStartupHook(startupHooks);
-            _scene.appendTeardownHook(teardownHooks);
+			_scene.appendStartupHook(startupHooks);
+			_scene.appendTeardownHook(teardownHooks);
+
+			return this;
+		}
+
+		// Custom builder methods here, make sure they return this (SceneBuilder)
+
+		public SceneBuilder appendStartupHook(Action hook) {
+			_scene.appendStartupHook(hook);
+
+			return this;
+		}
+
+		public SceneBuilder appendTeardownHook(Action hook) {
+			_scene.appendTeardownHook(hook);
+
+			return this;
+		}
+
+		public SceneBuilder resetScoreOnLeave() {
+			_scene.appendTeardownHook(GameManager.getInstance().resetScore);
 
             return this;
-        }
-
-        // Custom builder methods here, make sure they return this (SceneBuilder)
-
-        public SceneBuilder appendStartupHook(Action hook) {
-            _scene.appendStartupHook(hook);
-
-            return this;
-        }
-
-        public SceneBuilder appendTeardownHook(Action hook) {
-            _scene.appendTeardownHook(hook);
-
-            return this;
-        }
-    }
+		}
+	}
 }
