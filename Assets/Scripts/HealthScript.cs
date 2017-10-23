24c0d2a98bfd6b62cb117ce0935acc6b88632359
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2017-10-24 05:17:59 +1300

    did commenting

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This script is for controling health UI
+ * */
 public class HealthScript : MonoBehaviour {
 
 	public int health;
 	public Renderer renderers;
 
-	// Use this for initialization
+	// Use this for initialization, initialise the renderer of the hearts
 	void Start () {
 		renderers = GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//if the current health is less than what the heart should be displaying, hide it, if it is equal show it
 		if (POCC.GameManager.getInstance ().getHealth () < health) {
 			renderers.enabled = false;
 		} else if (POCC.GameManager.getInstance ().getHealth () == health) {
--- Assets/Scripts/TextScript.cs
+++ Assets/Scripts/TextScript.cs
@@ -3,18 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
+/**
+ * This script is for controlling the score UI.
+ * */
 public class TextScript : MonoBehaviour {
 
 	Text score;
 
 	// Use this for initialization
 	void Start () {
+		//Initialise the score as the current score.
 		score = GetComponent<Text> ();
 		score.text = "" + (int)POCC.GameManager.getInstance().getCollectableScore();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//for every frame update the score to what the current score is.
 		score.text = "" + (int)POCC.GameManager.getInstance().getCollectableScore();
 	}
 }
