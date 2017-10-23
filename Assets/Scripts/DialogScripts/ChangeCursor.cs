6a0b6494d173da9cd8c1de18ddd8d9679119ad25
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2017-10-23 18:30:50 +1300

    UI-redesign linking scenes

--- Assets/Scripts/DialogScripts/ChangeCursor.cs
+++ Assets/Scripts/DialogScripts/ChangeCursor.cs
@@ -7,18 +7,18 @@ public class ChangeCursor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		cursor = Resources.Load<Texture2D> ("chat");
-		cursor.Resize (5, 5);
-		cursor.width = 4;
+		//cursor = Resources.Load<Texture2D> ("chat");
+		//cursor.Resize (5, 5);
+		//cursor.width = 4;
 	
 	}
 	
 	void OnMouseOver(){
-		Cursor.SetCursor (cursor, Vector2.zero, CursorMode.Auto);
+		//Cursor.SetCursor (cursor, Vector2.zero, CursorMode.Auto);
 	}
 
 
 	void OnMouseExit(){
-		Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
+		//Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
 	}
 }
