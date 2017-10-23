4613a97d440a85d9e0ade48c63145c492e87540d
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-10-24 05:13:22 +1300

    Commented TongueSwing.cs

--- Assets/Scripts/Player/TongueSwing.cs
+++ Assets/Scripts/Player/TongueSwing.cs
@@ -7,40 +7,37 @@ using System.Collections;
  */
 public class TongueSwing : MonoBehaviour {
 
-	Vector3 anchor_position;
-	SpringJoint2D joint;
-	RaycastHit2D raycast_hit;
-	public LayerMask layer_mask;
-	public float distance = 3f;
-	public Transform anchor;
-	public LineRenderer rope;
+	Vector3 anchor_position; 		//Position of the anchor of the swing
+	SpringJoint2D joint;			//Joint constraining player and swing
+	RaycastHit2D raycast_hit;		//Variable representing the collision of raycast between anchor and player
+	public LayerMask layer_mask;	//Layer pertaining to swing ranges
+	public float distance = 3f;		//Distance of the joint
+	public Transform anchor;		//XY position of the anchor
+	public LineRenderer rope;		//Rope to render line
 
-	// Use this for initialization
+	// Initialisation of the class, components and first values.
 	void Start () {
 		joint = GetComponent<SpringJoint2D>();
 		joint.enabled = false;
 		anchor_position = anchor.position;
 
 		rope.enabled = false;
-
-		Debug.Log (anchor_position);
-
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-
+		//If in range then SPACE will make hte rope
 		if (Input.GetKeyDown(KeyCode.Space)){
 			anchor_position = anchor.position;
 
+			//Do raycast and get collision values
 			raycast_hit = Physics2D.Raycast (transform.position, 
 				anchor_position - transform.position,
 				distance,
 				layer_mask);
-			Debug.Log (raycast_hit.collider != null);
-			Debug.Log (transform.position + " " + anchor_position + " " + (raycast_hit.collider != null));
 
+			//If valid collison then create the joint and rope
 			if (raycast_hit.collider != null 
 				&& raycast_hit.collider.gameObject.GetComponent<Rigidbody2D>() != null){
 				joint.enabled = true;
@@ -54,10 +51,12 @@ public class TongueSwing : MonoBehaviour {
 			
 		}
 
+		//Update rope position of extended per frame
 		if (Input.GetKey(KeyCode.Space) && rope.enabled == true){
 			rope.SetPosition (0, transform.position);
 		}
 
+		//Disable rope when key up
 		if (Input.GetKeyUp(KeyCode.Space)){
 			joint.enabled = false;
 			rope.enabled = false;
