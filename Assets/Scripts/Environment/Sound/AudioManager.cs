7d07e8a5be8f8ead1fe1fc306b049f7280a3a7ac
Eli Salter <esal284@aucklanduni.ac.nz>
2017-10-23 20:31:05 +1300

    Tutorial level sound + AudioManager update (#112)
    * Added basic Audio Manager that loops through all sounds in a list and instatiates AudioSource objects of them
    
    * Created a basic audio manager, that looks after sound objects, audio manager prefab should be in every scene
    
    * Cleaned up working directory
    
    * Integrating new changes from master
    
    * Fleshed out sound manager to use sounds from 3rd party's for main menu and tutorial scene
    
    * Added sound to tutorial level and updated audio manager

---
+++ Assets/Scripts/Environment/Sound/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Credit: https://www.youtube.com/watch?v=6OT43pvUyfY&t=41s
+/// </summary>
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    private ArrayList currentlyPlaying = new ArrayList();
+
+    public static AudioManager instance;
+
+	// Use this for initialization
+	void Awake () {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Can't find (play) name of sound: " + name);
+        }
+        s.source.Play();
+        currentlyPlaying.Add(s);
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Can't find (stop) name of sound: " + name);
+        }
+        s.source.Stop();
+        currentlyPlaying.Remove(s);
+    }
+
+    public void StopAll() {
+        foreach(Sound s in currentlyPlaying)
+        {
+            s.source.Stop();
+        }
+        currentlyPlaying.Clear();
+    }
+}
---
+++ Assets/Scripts/Environment/Sound/tutorialLightMusic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorialLightMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        FindObjectOfType<AudioManager>().Stop("waterSound");
+        FindObjectOfType<AudioManager>().Stop("mainTheme");
+
+        FindObjectOfType<AudioManager>().Play("tutorialLight");
+        
+    }
+	
+	
+}
