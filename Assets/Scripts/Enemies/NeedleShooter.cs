6a4b8b446c16d72a54f655d8da92f56be4ab9f6b
Seoyoung Choi <seoyoungc@gmail.com>
2017-10-24 01:14:08 +1300

    Added comments to scripts: Needle, NeedleShooter, and both MovingPlatforms

--- Assets/Scripts/Enemies/NeedleShooter.cs
+++ Assets/Scripts/Enemies/NeedleShooter.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * This needleshooter was unfortunately not used in the final product.
+ * It instantiates a needle at a given rate.
+ */
 public class NeedleShooter : MonoBehaviour {
 
+	// The needle projectile that is instantianted
 	public GameObject projectile;
 
+	// The time between each shot
 	public float shootingRate;
+
+	//The counter is the timer that increments down every update call. 
 	float shootingRateCounter;
-	// Use this for initialization
+
+	// Sets the shooting rate counter to the rate, to start the down counter
 	void Start () {
 		shootingRateCounter = shootingRate;
 	}
 	
-	// Update is called once per frame
+	// Decrements the counter by the time passed, delta time.
+	// Once it reaches 0, instantiates the needle and fires, before resetting the counter back to the set rate.
 	void Update () {
 		shootingRateCounter -= Time.deltaTime;
 		if (shootingRateCounter <= 0.0f) {
